c372cd4a66eda51e84896be0fec9e4babde9d5f1
Petr Brazdil <p.brazdil@gmail.com>
2015-08-12 16:22:49 +0100

    Updated to use notifications

--- break.js
+++ break.js
@@ -10,9 +10,15 @@ breakUntil.setMinutes(breakUntil.getMinutes() + minutes);
 var untilStr = breakUntil.toTimeString().split(' ');
 untilStr = untilStr[0];
 
-shell.echo('Break until ' + untilStr);
+shell.exec("osascript -e 'display notification \"until " + untilStr + "\" with title \"Break\"'");
 shell.exec('open focus://unfocus');
 
 setTimeout(function() {
+  shell.exec("osascript -e 'display notification \"get back to work!\" with title \"Break ended\"'");
   shell.exec('open focus://focus');
 }, minutes * 1000 * 60);
+
+process.on('SIGINT', function() {
+    shell.exec('open focus://focus');
+    process.exit();
+});
